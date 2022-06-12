5571eea7c8620d122f184d9febc382b7e974b711
Sami <sami_khalid@hotmail.co.uk>
2022-06-12 04:08:07 +0100

    first commit

---
+++ InventoryTracking/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace InventoryTracking.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
